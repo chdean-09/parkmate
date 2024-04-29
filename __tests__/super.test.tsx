c4dc3d5102b45bb4cb353012cfe58d2ef77849fb
chdean-09 <chadandrada20@gmail.com>
2024-04-29 13:55:38 +0800

    update the GET request

--- __tests__/super.test.tsx
+++ __tests__/super.test.tsx
@@ -6,7 +6,7 @@ import { GET } from "@/app/api/test/[testnum]/route";
 
 describe("tests the nextjs route handler", () => {
   it("should show the number", async () => {
-    const res = await GET({ params: { testnum: 35345353 } });
+    const res = await GET({} as Request, { params: { testnum: 35345353 } });
     const body = await res.json();
 
     expect(res.status).toBe(200);
--- src/app/api/test/[testnum]/route.ts
+++ src/app/api/test/[testnum]/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { testnum: number } }) {
+export async function GET(
+  request: Request,
+  { params }: { params: { testnum: number } },
+) {
   return NextResponse.json(
     {
       number: params.testnum,
