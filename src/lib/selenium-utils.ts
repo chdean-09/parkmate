b4440c00f1da4ebdfe7e193e2c2503dfd47ceb86
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2024-05-25 02:16:48 +0800

    update selenium-utils with trycatch to handle error

--- src/lib/selenium-utils.ts
+++ src/lib/selenium-utils.ts
@@ -1,18 +1,36 @@
-import { until, By, WebDriver } from "selenium-webdriver";
+import { until, By, WebDriver, WebElement } from "selenium-webdriver";
 
-const waitUntilTime = 5 * 1000; // millisecond
+export async function getElementById(
+  id: string,
+  driver: WebDriver,
+  waitTime: number = 5000 // Default wait time in milliseconds
+): Promise<WebElement> {
+  try {
+    const el = await driver.wait(until.elementLocated(By.id(id)), waitTime);
+    await driver.wait(until.elementIsVisible(el), waitTime);
 
-export async function getElementById(id: string, driver: WebDriver) {
-  const el = await driver.wait(until.elementLocated(By.id(id)), waitUntilTime);
-
-  return await driver.wait(until.elementIsVisible(el), waitUntilTime);
+    return el;
+  } catch (error: any) {
+    throw new Error(`Error locating element with ID "${id}": ${error.message}`);
+  }
 }
 
-export async function getElementByXPath(xpath: string, driver: WebDriver) {
-  const el = await driver.wait(
-    until.elementLocated(By.xpath(xpath)),
-    waitUntilTime,
-  );
+export async function getElementByXPath(
+  xpath: string,
+  driver: WebDriver,
+  waitTime: number = 5000 // Default wait time in milliseconds
+): Promise<WebElement> {
+  try {
+    const el = await driver.wait(
+      until.elementLocated(By.xpath(xpath)),
+      waitTime
+    );
+    await driver.wait(until.elementIsVisible(el), waitTime);
 
-  return await driver.wait(until.elementIsVisible(el), waitUntilTime);
+    return el;
+  } catch (error: any) {
+    throw new Error(
+      `Error locating element with XPath "${xpath}": ${error.message}`
+    );
+  }
 }
