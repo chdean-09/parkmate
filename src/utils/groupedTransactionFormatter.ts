54d4c67e512ec4a2d387e934cd530b6c877905ec
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2024-05-26 15:47:50 +0800

    Reapply "Merge branch 'main' into chore/unit-tests-on-wallet-related"
    This reverts commit e2db787ebc6c35195d689f03556709be038cefba.

--- src/utils/groupedTransactionFormatter.ts
+++ src/utils/groupedTransactionFormatter.ts
@@ -6,12 +6,14 @@ type GroupedTransactions = {
   };
 };
 
-const groupTransactionFormatter = (transactions: Transaction[]): GroupedTransactions => {
+const groupTransactionFormatter = (
+  transactions: Transaction[],
+): GroupedTransactions => {
   return transactions.reduce((acc, transaction) => {
     const date = new Date(transaction.createdAt);
     const year = date.getFullYear().toString();
-    const month = date.toLocaleString('default', { month: 'long' });
-    const day = date.getDate().toString().padStart(2, '0');
+    const month = date.toLocaleString("default", { month: "long" });
+    const day = date.getDate().toString().padStart(2, "0");
     const monthDay = `${month} ${day}`;
 
     if (!acc[year]) acc[year] = {};
