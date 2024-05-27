ad8225b231e721340b15dd72df1e0208e11c8976
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2024-05-27 22:45:49 +0800

    fixed flaky test on selenium login signup

--- __tests__/selenium/unit-test/signup.test.tsx
+++ __tests__/selenium/unit-test/signup.test.tsx
@@ -1,12 +1,9 @@
-import { Builder, WebDriver, By, WebElement } from "selenium-webdriver";
+import { Builder, until, WebDriver, WebElement } from "selenium-webdriver";
 import "selenium-webdriver/chrome";
 import "chromedriver";
 import "selenium-webdriver/firefox";
 import "geckodriver";
-import {
-  getElementById,
-  getElementByXPath,
-} from "../../../src/lib/selenium-utils";
+import { getElementById } from "@/lib/selenium-utils";
 
 const rootURL = "http://localhost:3000/signup";
 
@@ -19,46 +16,40 @@ describe("Sign up page", () => {
   beforeAll(async () => {
     driver = await new Builder().forBrowser("firefox").build();
     await driver.get(rootURL);
-  }, 10000);
+  }, 20000);
 
   afterAll(async () => {
     await driver.quit();
-  }, 10000);
+  }, 20000);
 
-  it("check if sign up form is displayed", async () => {
+  it("checks if sign up form is displayed", async () => {
     const signUpForm = await getElementById("signup-form", driver);
-
-    expect(signUpForm.isDisplayed).toBeTruthy();
+    expect(await signUpForm.isDisplayed()).toBeTruthy();
   });
 
-  it("must have sign up greetings and app name displayed", async () => {
+  it("displays sign up greetings and app name", async () => {
     const appName = await getElementById("app-name-greet", driver);
-
     const textHeader = await appName.getText();
     const expectedHeaderText = "Welcome To ParkMate!";
     expect(textHeader).toEqual(expectedHeaderText);
   });
 
-  it("must have sign up text displayed", async () => {
+  it("displays sign up text", async () => {
     const signUpEl = await getElementById("signup-text", driver);
-
     const signUpText = await signUpEl.getText();
     const expectedSignUpText = "Sign up to continue";
     expect(signUpText).toEqual(expectedSignUpText);
   });
 
-  it("must have sign up button displayed", async () => {
+  it("displays sign up button", async () => {
     const signUpButton = await getElementById("signup-button-active", driver);
-
-    expect(signUpButton.isDisplayed).toBeTruthy();
+    expect(await signUpButton.isDisplayed()).toBeTruthy();
   });
 
-  describe("User puts incorrect credentials", () => {
+  describe("User enters incorrect credentials", () => {
     let usernameField: WebElement;
     let passwordField: WebElement;
     let signUpButton: WebElement;
-    let usernameText: string;
-    let passwordText: string;
 
     beforeEach(async () => {
       usernameField = await getElementById("username-field", driver);
@@ -73,94 +64,75 @@ describe("Sign up page", () => {
 
     it("displays error message if username is less than 3 characters", async () => {
       await usernameField.sendKeys("se");
-      await passwordField.sendKeys("seleniumtestpassword");
-      const usernameText = await usernameField.getAttribute("value");
-      const passwordText = await passwordField.getAttribute("value");
-
-      const expectedUsername = "se";
-      expect(usernameText).toEqual(expectedUsername);
-      const expectedPassword = "seleniumtestpassword";
-      expect(passwordText).toEqual(expectedPassword);
-
-      signUpButton.click();
+      await passwordField.sendKeys(password);
+      driver.executeScript("arguments[0].click();", signUpButton);
+      // await signUpButton.click();
 
       const errorElement = await getElementById("error-message", driver);
       const errorText = await errorElement.getText();
-
       expect(errorText).toEqual("Username must be at least 3 characters.");
     }, 10000);
 
     it("displays error message if password is less than 6 characters", async () => {
-      await usernameField.sendKeys("seleniumtest");
+      await usernameField.sendKeys(username);
       await passwordField.sendKeys("se");
-      const usernameText = await usernameField.getAttribute("value");
-      const passwordText = await passwordField.getAttribute("value");
+      
+      // await signUpButton.click();
+      driver.executeScript("arguments[0].click();", signUpButton)
 
-      const expectedUsername = "seleniumtest";
-      expect(usernameText).toEqual(expectedUsername);
-      const expectedPassword = "se";
-      expect(passwordText).toEqual(expectedPassword);
-
-      signUpButton.click();
 
       const errorElement = await getElementById("error-message", driver);
       const errorText = await errorElement.getText();
-
       expect(errorText).toEqual("Password must be at least 6 characters");
     }, 10000);
 
-    it("displays error message if username already exists", async () => {
-      await usernameField.sendKeys("seleniumtest");
+    it("displays error message if username already exists but wrong password", async () => {
+      await usernameField.sendKeys(username);
       await passwordField.sendKeys("seleniumtestwrongpassword");
-      const usernameText = await usernameField.getAttribute("value");
-      const passwordText = await passwordField.getAttribute("value");
+      // await signUpButton.click();
+      driver.executeScript("arguments[0].click();", signUpButton)
 
-      const expectedUsername = "seleniumtest";
-      expect(usernameText).toEqual(expectedUsername);
-      const expectedPassword = "seleniumtestwrongpassword";
-      expect(passwordText).toEqual(expectedPassword);
-
-      signUpButton.click();
 
       const errorElement = await getElementById("error-message", driver);
       const errorText = await errorElement.getText();
-
-      expect(errorText).toEqual("Username already exists");
+      expect(errorText).toEqual("Incorrect username or password");
     }, 10000);
   });
 
-  describe("User puts correct credentials", () => {
+  describe("User enters correct credentials", () => {
     it("signs up user after filling out form", async () => {
       const usernameField = await getElementById("username-field", driver);
       const passwordField = await getElementById("password-field", driver);
       const signUpButton = await getElementById("signup-button-active", driver);
 
-      await usernameField.sendKeys("seleniumtest");
+      await usernameField.sendKeys(username);
+      await passwordField.sendKeys(password);
+
       const usernameText = await usernameField.getAttribute("value");
-      await passwordField.sendKeys("seleniumtestpassword");
       const passwordText = await passwordField.getAttribute("value");
 
-      const expectedUsername = username;
-      expect(usernameText).toEqual(expectedUsername);
-      const expectedPassword = password;
-      expect(passwordText).toEqual(expectedPassword);
+      expect(usernameText).toEqual(username);
+      expect(passwordText).toEqual(password);
 
-      signUpButton.click();
+      // await signUpButton.click();
+      driver.executeScript("arguments[0].click();", signUpButton)
 
       const appNameElement = await getElementById("app-title", driver);
       const text = await appNameElement.getText();
-
       expect(text).toEqual("Parkmate");
     }, 20000);
 
-    it("Log out user after signing up", async () => {
-      const appNameElement = await getElementById("app-title", driver);
-      const text = await appNameElement.getText();
-
-      expect(text).toEqual("Parkmate");
-
+    it("logs out user after signing up", async () => {
       const logoutButton = await getElementById("logout-btn", driver);
-      await logoutButton.click();
+
+      await driver.wait(until.elementIsEnabled(logoutButton), 10000);
+      // await logoutButton.click();
+      driver.executeScript("arguments[0].click();", logoutButton)
+
+      const loginGreetElement = await getElementById("login-greet", driver);
+      const loginGreetText = await loginGreetElement.getText();
+      const expectedLoginGreetText = "Welcome To ParkMate!";
+      expect(loginGreetText).toEqual(expectedLoginGreetText);
     }, 10000);
   });
 });
